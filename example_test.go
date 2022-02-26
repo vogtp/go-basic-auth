5d619592ccafb134d095de4eac35921c115f7077
Patrick Vogt <patrick.vogt.pv@gmail.com>
2022-02-26 20:42:51 +0100

    typo

--- example_test.go
+++ example_test.go
@@ -15,7 +15,7 @@ func ExampleHandleFunc() {
 		auth.WithLdap("SERVER_NAME", "BASE_DN", "DOMAIN_NAME"), // authorise users for (AD) LDAP
 		auth.WithGroup("group"),                                // One or more groups the user has to be in to be authorised
 		auth.Debug(),                                           // enable debug output
-		auth.WithFailMsg("Use the email adress as user name"))  // custom error message
+		auth.WithFailMsg("Use the email address as user name")) // custom error message
 	// use as http middleware
 	http.HandleFunc("/", basicAuth.Handler(http.NotFound))
 	// or use as GIN middleware
